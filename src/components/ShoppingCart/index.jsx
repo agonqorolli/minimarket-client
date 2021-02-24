714d97bd91cccbaa40fb5f13b47ca07fd1253ed5
Agon Qorolli <agon@storychief.io>
2021-02-24 12:41:38 +0100

    fix base path

--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -56,9 +56,9 @@ function ShoppingCart() {
   async function handleOnBuyClick(event) {
     event.preventDefault();
 
-    const BASE_PATH = process.env.NODE_ENV === 'production'
-      ? 'https://main.d2jsglc8ny66a.amplifyapp.com/'
-      : 'http://localhost:3000'
+    const BASE_PATH = process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://main.d2jsglc8ny66a.amplifyapp.com/';
 
     getStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_TEST_KEY).then(stripe => {
       stripe.redirectToCheckout({
